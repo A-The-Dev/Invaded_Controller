448a34991b2fcfd68b92d8efba20944a622dcc0d
TiDanick28 <dangagne28@gmail.com>
2026-02-22 20:33:08 -0500

    on a rajotuer le squellette avec l encodeur donc c un debut

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,91 @@
 #include <Arduino.h>
 #include "acelerometre.h"
 #include "bouton.h"
+#include "incremental.h"
+
+int lastValue = 0;
+int menu = 0;
 
 void setup()
 {
   Serial.begin(9600);
   //Serial.print("Debut de l'initialisation de l'accelerometre");
+  setup_encoder(2, 3);  // use interrupt pin 2 for channel A and pin 3 for channel B
+  
 }
 
 void loop()
 {
-  setup_accelerometre();
+  //setup_accelerometre();
   //setup_bouton();
+ 
+ 
+  //bouton du rotary encoder
+
+  /*
+  if (digitalRead(4) == true)
+  {
+    Serial.println("Bouton appuye");
+  }
+  else if (digitalRead(4) == false)
+  {
+    Serial.println("Bouton relache");
+  }
+  */
+
+  int currentValue = read_encoder();
+  
+
+  
+  if (currentValue > lastValue)
+  {
+    menu++;
+    if (menu > 2){
+      menu = 0;
+    }
+  
+  }
+  else if (currentValue < lastValue)
+  {
+    menu--;
+    if (menu < 0){
+      menu = 2;
+    }
+  }
+  lastValue = currentValue;
+  Serial.println(currentValue);
+  Serial.println(lastValue);
+  if (menu == 0){
+    Serial.println("ACCELEROMETRE");
+  }
+  else if (menu == 1){
+    Serial.println("BOUTON");
+  }
+  else if (menu == 2){
+    Serial.println("LEBRON JAMES");
+  }
+  
+
+
+
+  /*if (currentValue == lastValue){
+    Serial.println("Pas de changement");
+  }
+  else if (currentValue > lastValue)
+  {
+    Serial.println("Rotation antihoraire");
+    Serial.print("Valeur actuelle: ");
+    Serial.println(currentValue);
+    lastValue = currentValue;
+  }
+  else if (currentValue < lastValue)
+  {
+    Serial.println("Rotation horaire");
+    Serial.print("Valeur actuelle: ");
+    Serial.println(currentValue);
+    lastValue = currentValue;
+  }*/
+  delay (1000);
 }
 
 
