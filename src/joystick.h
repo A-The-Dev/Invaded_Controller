4c9cffbf31866bab8b43e8e3623bb2e12d2399fe
XavierDesjardins <desx4114@uSherbrooke.ca>
2026-03-06 14:51:35 -0500

    separation du joystick en deux fichier

--- src/joystick.h
+++ src/joystick.h
@@ -4,60 +4,19 @@
 // bseoin d'inclure JoyInit dans le setup
 // dans le loop appeler JoyPrint
 
-#include <Arduino.h>
-#include <stdbool.h>
-
 #define pinX 8//nb 
-#define pinY 10//nb
-
-float maxX = 100;  // À modifier et rentrer la valeur maximal de X  
-float maxY = 200;  // À modifier et rentrer la valeur maximal de Y
+#define pinY 10// 
 
 
-void JoyInit(){
-    pinMode(pinX, INPUT); //entré x
-    pinMode(pinY, INPUT); //entrée y 
-}
 
-float cordX(){
-    float joyX = analogRead(pinX);
-    return (joyX/maxX);     //change les valeur pour etre entre -1 et 1
-}
-
-float cordY(){
-    float joyY = analogRead(pinY);
-    return (joyY/maxY);     //change les valeur pour etre entre -1 et 1
-}
-
-float JoyCalibX(){
-    float joyX = analogRead(pinX);
-    Serial.print(joyX);             // a utiliser pour le max X
-}
-
-float JoyCalibY(){
-    float joyY = analogRead(pinY);
-    Serial.print(joyY);             // a utiliser pour le max Y
-}
-
-
-void JoyPrintCalib(){
-    Serial.print("X Calib");
-    Serial.print(JoyCalibX());    //print les valeur entre -1 et 1 de X
-    Serial.print("Y Calib");
-    Serial.print(JoyCalibY());    //print les valeur entre -1 et 1 de Y
-
-}
-
-void JoyPrint(){
-    Serial.print("X");
-    Serial.print(JoyCalibX());
-    Serial.print("y");
-    Serial.print(JoyCalibX());
-}
-
-void JoyCom(){
-// je ne sais pas encore comment faire la comunication encore
-}
+void JoyInit();
+float cordX();
+float cordY();
+float JoyCalibX();
+float JoyCalibY();
+void JoyPrintCalib();
+void JoyPrint();
+void JoyCom();
 
 
 #endif
